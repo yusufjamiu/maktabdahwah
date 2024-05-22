ea388406584e6188ad131d9ff3aca543175f0af2
yusufjamiu <princeyusufjamiuadegbolahan@gmail.com>
2024-05-22 09:20:16 +0100

    Changes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,18 +3,32 @@ import { BsArrowLeftShort } from 'react-icons/bs';
 import logoImage from '../assets/Maktab.JPG';
 
 const Sidebar = () => {
-  const [open, setOpen] = useState(true); // Initialize open state with a default value
+  const [open, setOpen] = useState(true);
 
   return (
     <div className="flex">
-      <div className={`bg-slate-600 h-screen p-5 pt-8 ${open ? 'w-72' : 'w-20'} duration-300 relative`}>
+      <div
+        className={`bg-slate-600 h-screen p-5 pt-8 ${
+          open ? 'w-72 sm:w-40' : 'w-20 sm:15'
+        } duration-300 relative`}
+      >
         <BsArrowLeftShort
-          className={`bg-white text-dark-green text-3xl rounded-full absolute -right-3 top-9 border border-dark-yellow cursor-pointer ${!open && 'rotate-180'}`}
+          className={`bg-white text-dark-green text-3xl rounded-full absolute -right-3 top-9 border border-dark-yellow cursor-pointer ${
+            !open && 'rotate-180'
+          }`}
           onClick={() => setOpen(!open)}
         />
         <div className="inline-flex items-center">
-          <img src={logoImage} alt="Logo" className={`h-16 mr-2 duration-500 ${open && 'rotate-[360deg]'}`} />
-          <span className={`text-white origin-left font-medium text-2xl duration-300 ${!open && 'scale-0'}`}>
+          <img
+            src={logoImage}
+            alt="Logo"
+            className={`h-16 mr-2 duration-500 ${open && 'rotate-[360deg]'}`}
+          />
+          <span
+            className={`text-white origin-left font-medium text-2xl duration-300 ${
+              !open && 'scale-0'
+            }`}
+          >
             MaktabDahwah
           </span>
         </div>
