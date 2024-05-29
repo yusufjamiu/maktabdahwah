a1a5a28f3e51610b11bec14849aa4c0d9e4aa664
yusufjamiu <princeyusufjamiuadegbolahan@gmail.com>
2024-05-29 12:28:32 +0100

    Changes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,10 +5,20 @@ import logoImage from '../assets/Maktab.JPG';
 
 const Sidebar = () => {
   const [open, setOpen] = useState(true);
-  const [activeMenu, setActiveMenu] = useState(null);
+  const [homeOpen, setHomeOpen] = useState(false);
+  const [lecturesOpen, setLecturesOpen] = useState(false);
+  const [ramadanOpen, setRamadanOpen] = useState(false);
+  const [quranOpen, setQuranOpen] = useState(false);
+  const [eventsOpen, setEventsOpen] = useState(false);
+  const [scholarshipsOpen, setScholarshipsOpen] = useState(false);
 
   const toggleSubmenu = (menu) => {
-    setActiveMenu(activeMenu === menu ? null : menu);
+    setHomeOpen(menu === 'home' ? !homeOpen : false);
+    setLecturesOpen(menu === 'lectures' ? !lecturesOpen : false);
+    setRamadanOpen(menu === 'ramadan' ? !ramadanOpen : false);
+    setQuranOpen(menu === 'quran' ? !quranOpen : false);
+    setEventsOpen(menu === 'events' ? !eventsOpen : false);
+    setScholarshipsOpen(menu === 'scholarships' ? !scholarshipsOpen : false);
   };
 
   return (
@@ -21,7 +31,7 @@ const Sidebar = () => {
         <div className="inline-flex items-center">
           <img src={logoImage} alt="Logo" className={`h-16 mr-2 duration-500 ${open && 'rotate-[360deg]'}`} />
           <span className={`text-white origin-left font-medium text-2xl duration-300 ${!open && 'scale-0'}`}>
-            MaktabDahwah
+            MaktabNigeria
           </span>
         </div>
         <div className={`flex items-center rounded-md bg-gray-400 mt-6 ${!open ? 'px-2.5' : 'px-4'} py-2`}>
@@ -36,150 +46,145 @@ const Sidebar = () => {
           <div className={`flex items-center rounded-md ${!open ? 'px-2.5' : 'px-4'} py-2`}>
             <Link
               to="/"
-              className={`flex items-center text-white rounded-md p-2 hover:bg-gray-700 w-full`}
+              className={`flex items-center text-white hover:bg-gray-700 rounded-md p-2 ${homeOpen && 'bg-gray-700'}`}
               onClick={() => toggleSubmenu('home')}
             >
               <BsHouse className={`text-lg mr-2 ${!open && 'mx-auto'}`} />
-              <span className={`${!open && 'hidden'}`}>Home</span>
+              <span className={`${!open && 'hidden'}`}>Home / الصفحة الرئيسية</span>
             </Link>
           </div>
-          <div className={`flex flex-col rounded-md ${!open ? 'px-2.5' : 'px-4'} py-2`}>
-            <Link
-              to="/lectures"
-              className={`flex items-center text-white rounded-md p-2 hover:bg-gray-700 w-full`}
+          <div className={`flex items-center rounded-md ${!open ? 'px-2.5' : 'px-4'} py-2`}>
+            <div
+              className={`flex items-center text-white hover:bg-gray-700 rounded-md p-2 ${lecturesOpen && 'bg-gray-700'}`}
               onClick={() => toggleSubmenu('lectures')}
             >
               <BsMusicNoteList className={`text-lg mr-2 ${!open && 'mx-auto'}`} />
-              <span className={`${!open && 'hidden'}`}>Lectures</span>
-            </Link>
-            {activeMenu === 'lectures' && (
+              <span className={`${!open && 'hidden'}`}>Lectures / محاضرات</span>
+            </div>
+            {lecturesOpen && (
               <div className={`pl-6 ${!open && 'hidden'}`}>
                 <Link to="/lectures/lecture1" className="block text-white hover:bg-gray-700 rounded-md p-2">
-                  Lecture 1
+                  Lecture 1 / محاضرة 1
                 </Link>
                 <Link to="/lectures/lecture2" className="block text-white hover:bg-gray-700 rounded-md p-2">
-                  Lecture 2
+                  Lecture 2 / محاضرة 2
                 </Link>
                 <Link to="/lectures/lecture3" className="block text-white hover:bg-gray-700 rounded-md p-2">
-                  Lecture 3
+                  Lecture 3 / محاضرة 3
                 </Link>
               </div>
             )}
           </div>
-          <div className={`flex flex-col rounded-md ${!open ? 'px-2.5' : 'px-4'} py-2`}>
-            <Link
-              to="/ramadan"
-              className={`flex items-center text-white rounded-md p-2 hover:bg-gray-700 w-full`}
+          <div className={`flex items-center rounded-md ${!open ? 'px-2.5' : 'px-4'} py-2`}>
+            <div
+              className={`flex items-center text-white hover:bg-gray-700 rounded-md p-2 ${ramadanOpen && 'bg-gray-700'}`}
               onClick={() => toggleSubmenu('ramadan')}
             >
               <BsCalendar className={`text-lg mr-2 ${!open && 'mx-auto'}`} />
-              <span className={`${!open && 'hidden'}`}>Ramadan</span>
-            </Link>
-            {activeMenu === 'ramadan' && (
+              <span className={`${!open && 'hidden'}`}>Ramadan / رمضان</span>
+            </div>
+            {ramadanOpen && (
               <div className={`pl-6 ${!open && 'hidden'}`}>
                 <Link to="/ramadan/schedule" className="block text-white hover:bg-gray-700 rounded-md p-2">
-                  Schedule
+                  Schedule / جدول
                 </Link>
                 <Link to="/ramadan/activities" className="block text-white hover:bg-gray-700 rounded-md p-2">
-                  Activities
+                  Activities / أنشطة
                 </Link>
                 <Link to="/ramadan/resources" className="block text-white hover:bg-gray-700 rounded-md p-2">
-                  Resources
+                  Resources / مصادر
                 </Link>
               </div>
             )}
           </div>
-          <div className={`flex flex-col rounded-md ${!open ? 'px-2.5' : 'px-4'} py-2`}>
-            <Link
-              to="/quran"
-              className={`flex items-center text-white rounded-md p-2 hover:bg-gray-700 w-full`}
+          <div className={`flex items-center rounded-md ${!open ? 'px-2.5' : 'px-4'} py-2`}>
+            <div
+              className={`flex items-center text-white hover:bg-gray-700 rounded-md p-2 ${quranOpen && 'bg-gray-700'}`}
               onClick={() => toggleSubmenu('quran')}
             >
               <BsBook className={`text-lg mr-2 ${!open && 'mx-auto'}`} />
-              <span className={`${!open && 'hidden'}`}>Quran</span>
-            </Link>
-            {activeMenu === 'quran' && (
+              <span className={`${!open && 'hidden'}`}>Quran / القرآن</span>
+            </div>
+            {quranOpen && (
               <div className={`pl-6 ${!open && 'hidden'}`}>
                 <Link to="/quran/recitation" className="block text-white hover:bg-gray-700 rounded-md p-2">
-                  Recitation
+                  Recitation / تلاوة
                 </Link>
                 <Link to="/quran/translation" className="block text-white hover:bg-gray-700 rounded-md p-2">
-                  Translation
+                  Translation / ترجمة
                 </Link>
                 <Link to="/quran/tafsir" className="block text-white hover:bg-gray-700 rounded-md p-2">
-                  Tafsir
-                </Link>
-              </div>
-            )}
-          </div>
-          <div className={`flex flex-col rounded-md ${!open ? 'px-2.5' : 'px-4'} py-2`}>
-            <Link
-              to="/events"
-              className={`flex items-center text-white rounded-md p-2 hover:bg-gray-700 w-full`}
-              onClick={() => toggleSubmenu('events')}
-            >
-              <BsCalendarEvent className={`text-lg mr-2 ${!open && 'mx-auto'}`} />
-              <span className={`${!open && 'hidden'}`}>Events</span>
-            </Link>
-            {activeMenu === 'events' && (
-              <div className={`pl-6 ${!open && 'hidden'}`}>
-                <Link to="/events/upcoming" className="block text-white hover:bg-gray-700 rounded-md p-2">
-                  Upcoming
-                </Link>
-                <Link to="/events/past" className="block text-white hover:bg-gray-700 rounded-md p-2">
-                  Past
-                </Link>
-                <Link to="/events/register" className="block text-white hover:bg-gray-700 rounded-md p-2">
-                  Register
-                </Link>
-              </div>
-            )}
-          </div>
-          <div className={`flex flex-col rounded-md ${!open ? 'px-2.5' : 'px-4'} py-2`}>
-            <Link
-              to="/scholarships"
-              className={`flex items-center text-white rounded-md p-2 hover:bg-gray-700 w-full`}
-              onClick={() => toggleSubmenu('scholarships')}
-            >
-              <BsMortarboardFill className={`text-lg mr-2 ${!open && 'mx-auto'}`} />
-              <span className={`${!open && 'hidden'}`}>Scholarships</span>
-            </Link>
-            {activeMenu === 'scholarships' && (
-              <div className={`pl-6 ${!open && 'hidden'}`}>
-                <Link to="/scholarships/apply" className="block text-white hover:bg-gray-700 rounded-md p-2">
-                  Apply
-                </Link>
-                <Link to="/scholarships/requirements" className="block text-white hover:bg-gray-700 rounded-md p-2">
-                  Requirements
-                </Link>
-                <Link to="/scholarships/recipients" className="block text-white hover:bg-gray-700 rounded-md p-2">
-                  Recipients
+                  Tafsir / تفسير
                 </Link>
               </div>
             )}
           </div>
           <div className={`flex items-center rounded-md ${!open ? 'px-2.5' : 'px-4'} py-2`}>
-            <Link to="/blog" className="flex items-center text-white rounded-md p-2 hover:bg-gray-700 w-full">
-              <BsBook className={`text-lg mr-2 ${!open && 'mx-auto'}`} />
-              <span className={`${!open && 'hidden'}`}>Blog</span>
-            </Link>
-          </div>
-          <div className={`flex items-center rounded-md ${!open ? 'px-2.5' : 'px-4'} py-2`}>
-            <Link to="/solat-time" className="flex items-center text-white rounded-md p-2 hover:bg-gray-700 w-full">
-              <BsCalendar className={`text-lg mr-2 ${!open && 'mx-auto'}`} />
-              <span className={`${!open && 'hidden'}`}>Solat Time</span>
-            </Link>
-          </div>
-          <div className={`flex items-center rounded-md ${!open ? 'px-2.5' : 'px-4'} py-2`}>
-            <Link to="/calendar" className="flex items-center text-white rounded-md p-2 hover:bg-gray-700 w-full">
-              <BsMoonStarsFill className={`text-lg mr-2 ${!open && 'mx-auto'}`} />
-              <span className={`${!open && 'hidden'}`}>Calendar</span>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+           <div
+             className={`flex items-center text-white hover:bg-gray-700 rounded-md p-2 ${eventsOpen && 'bg-gray-700'}`}
+             onClick={() => toggleSubmenu('events')}
+           >
+             <BsCalendarEvent className={`text-lg mr-2 ${!open && 'mx-auto'}`} />
+             <span className={`${!open && 'hidden'}`}>Events / فعاليات</span>
+           </div>
+           {eventsOpen && (
+             <div className={`pl-6 ${!open && 'hidden'}`}>
+               <Link to="/events/upcoming" className="block text-white hover:bg-gray-700 rounded-md p-2">
+                 Upcoming / قادمة
+               </Link>
+               <Link to="/events/past" className="block text-white hover:bg-gray-700 rounded-md p-2">
+                 Past / ماضية
+               </Link>
+               <Link to="/events/register" className="block text-white hover:bg-gray-700 rounded-md p-2">
+                 Register / تسجيل
+               </Link>
+             </div>
+           )}
+         </div>
+         <div className={`flex items-center rounded-md ${!open ? 'px-2.5' : 'px-4'} py-2`}>
+           <div
+             className={`flex items-center text-white hover:bg-gray-700 rounded-md p-2 ${scholarshipsOpen && 'bg-gray-700'}`}
+             onClick={() => toggleSubmenu('scholarships')}
+           >
+             <BsMortarboardFill className={`text-lg mr-2 ${!open && 'mx-auto'}`} />
+             <span className={`${!open && 'hidden'}`}>Scholarships / منح دراسية</span>
+           </div>
+           {scholarshipsOpen && (
+             <div className={`pl-6 ${!open && 'hidden'}`}>
+               <Link to="/scholarships/apply" className="block text-white hover:bg-gray-700 rounded-md p-2">
+                 Apply / تقديم
+               </Link>
+               <Link to="/scholarships/requirements" className="block text-white hover:bg-gray-700 rounded-md p-2">
+                 Requirements / متطلبات
+               </Link>
+               <Link to="/scholarships/recipients" className="block text-white hover:bg-gray-700 rounded-md p-2">
+                 Recipients / المتلقين
+               </Link>
+             </div>
+           )}
+         </div>
+         <div className={`flex items-center rounded-md ${!open ? 'px-2.5' : 'px-4'} py-2`}>
+           <Link to="/blog" className="flex items-center text-white hover:bg-gray-700 rounded-md p-2">
+             <BsBook className={`text-lg mr-2 ${!open && 'mx-auto'}`} />
+             <span className={`${!open && 'hidden'}`}>Blog / مدونة</span>
+           </Link>
+         </div>
+         <div className={`flex items-center rounded-md ${!open ? 'px-2.5' : 'px-4'} py-2`}>
+           <Link to="/solat-time" className="flex items-center text-white hover:bg-gray-700 rounded-md p-2">
+             <BsCalendar className={`text-lg mr-2 ${!open && 'mx-auto'}`} />
+             <span className={`${!open && 'hidden'}`}>Solat Time / أوقات الصلاة</span>
+           </Link>
+         </div>
+         <div className={`flex items-center rounded-md ${!open ? 'px-2.5' : 'px -4'} py-2`}>
+           <Link to="/calendar" className="flex items-center text-white hover:bg-gray-700 rounded-md p-2">
+             <BsMoonStarsFill className={`text-lg mr-2 ${!open && 'mx-auto'}`} />
+             <span className={`${!open && 'hidden'}`}>Calendar / التقويم</span>
+           </Link>
+         </div>
+       </div>
+     </div>
+   </div>
+ );
 };
 
 export default Sidebar;
